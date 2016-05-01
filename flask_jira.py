eb49c07ff8e490c61a7bec04a6dd2697ed040091
Jiri Bires <kiri.cz@gmail.com>
2016-05-01 22:23:09 +0200

    Initial commit

---
+++ flask_jira.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from jira import JIRA
+
+# Find the stack on which we want to store the database connection.
+# Starting with Flask 0.9, the _app_ctx_stack is the correct one,
+# before that we need to use the _request_ctx_stack.
+try:
+    from flask import _app_ctx_stack as stack
+except ImportError:
+    from flask import _request_ctx_stack as stack
+
+
+class FlaskJIRA(object):
+    def __init__(self, app=None, **kwargs):
+        self.app = app
+        if app is not None:
+            self.init_app(app, **kwargs)
+
+    def init_app(self, app, **kwargs):
+        app.config.setdefault('JIRA_SERVER', None)
+        app.config.setdefault('JIRA_OPTIONS', None)
+        app.config.setdefault('JIRA_BASIC_AUTH', None)
+        app.config.setdefault('JIRA_OAUTH', None)
+        app.config.setdefault('JIRA_JWT', None)
+        app.config.setdefault('JIRA_KERBEROS', None)
+
+    def __getattr__(self, item):
+        ctx = stack.top
+        if ctx is not None:
+            if not hasattr(ctx, 'jira'):
+                ctx.jira = JIRA(server=ctx.app.config.get('JIRA_SERVER'),
+                                options=ctx.app.config.get('JIRA_OPTIONS'),
+                                basic_auth=ctx.app.config.get('JIRA_BASIC_AUTH'),
+                                oauth=ctx.app.config.get('JIRA_OAUTH'),
+                                jwt=ctx.app.config.get('JIRA_JWT'),
+                                kerberos=ctx.app.config.get('JIRA_KERBEROS'))
+
+
+            return getattr(ctx.jira, item)
+
+    def teardown(self, exception):
+        ctx = stack.top
+        if hasattr(ctx, 'jira'):
+            ctx.jira = None
---
+++ setup.py
@@ -0,0 +1,36 @@
+"""
+Flask-JIRA
+-------------
+
+Flask extension that provides simple integration with JIRA REST API.
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-JIRA',
+    version='0.0.1',
+    url='https://github.com/jiribires/flask-jira',
+    license='MIT',
+    author='Jiri Bires',
+    author_email='jiri.bires@gmail.com',
+    description='Flask extension that provides simple integration with JIRA REST API',
+    long_description=__doc__,
+    py_modules=['flask_jira'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'jira'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
